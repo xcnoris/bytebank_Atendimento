894d7c7058445ded3d9f5b3362d0efcabfb60625
xcnoris <augustobriel12@gmail.com>
2024-04-07 17:20:48 -0300

    outros

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -4,6 +4,23 @@ using bytebank_ATENDIMENTO.bytebank.Util;
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 //new ByteBankAtendimento().AtendimentoCliente();
 
+
+ContaCorrente conta1 = new ContaCorrente(777, "7654321-A");
+
+
+ContaCorrente conta2 = new ContaCorrente(777, "7654321-C");
+conta2.Depositar(10400);
+
+ContaCorrente conta3 = new ContaCorrente(777, "7654321-G");
+conta3.Depositar(120000);
+
+ContaCorrente conta4 = new ContaCorrente(777, "7654321-H");
+conta4.Depositar(13000);
+
+ContaCorrente conta5 = new ContaCorrente(777, "7654321-L");
+conta5.Depositar(5000);
+
+
 void TestaArrayDeContasCorrentes()
 {
     ListaDeContasCorrentes listaDeContas = new ListaDeContasCorrentes();
@@ -13,5 +30,23 @@ void TestaArrayDeContasCorrentes()
     listaDeContas.AdicionarConta(new ContaCorrente(877, "2347521-D"));
     listaDeContas.AdicionarConta(new ContaCorrente(877, "2347521-E"));
     listaDeContas.AdicionarConta(new ContaCorrente(877, "2347521-F"));
+    listaDeContas.AdicionarConta(conta1);
+    listaDeContas.AdicionarConta(conta2);
+    listaDeContas.AdicionarConta(conta3);
+    listaDeContas.AdicionarConta(conta4);
+    listaDeContas.AdicionarConta(conta5);
+    //listaDeContas.ExibirLista();
+    //Console.WriteLine("============================================");
+    //listaDeContas.Remover(conta1);
+    //listaDeContas.ExibirLista();
+
+    for (int i = 0; i < listaDeContas.Tamanho; i++)
+    {
+        ContaCorrente conta = listaDeContas[i];
+        Console.WriteLine($"Indice [{i}] = {conta.Conta}/{conta.Numero_agencia}");
+    }
+
+
+
 }
-TestaArrayDeContasCorrentes();
+//  TestaArrayDeContasCorrentes();
--- bytebank_ATENDIMENTO/bytebank.Util/ListaDeContasCorrentes.cs
+++ bytebank_ATENDIMENTO/bytebank.Util/ListaDeContasCorrentes.cs
@@ -10,18 +10,18 @@ namespace bytebank_ATENDIMENTO.bytebank.Util;
 public class ListaDeContasCorrentes
 {
     private ContaCorrente[] _itens = null;
-    private int _proximaposicao = 0;
-    public ListaDeContasCorrentes(int tamanhoInicial=5)
+    private int _proximaPosicao = 0;
+    public ListaDeContasCorrentes(int tamanhoInicial = 5)
     {
         _itens = new ContaCorrente[tamanhoInicial];
     }
 
     public void AdicionarConta(ContaCorrente item)
     {
-        Console.WriteLine($"Adicionanddo itens na posição {_proximaposicao}");
-        VerificarCapacidade(_proximaposicao + 1);
-        _itens[_proximaposicao] = item;
-        _proximaposicao++;
+        Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
+        VerificarCapacidade(_proximaPosicao + 1);
+        _itens[_proximaPosicao] = item;
+        _proximaPosicao++;
     }
 
     private void VerificarCapacidade(int tamanhoNecessario)
@@ -30,15 +30,78 @@ public class ListaDeContasCorrentes
         {
             return;
         }
-        else
+        Console.WriteLine("Aumentando a capacidade da lista!");
+        ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
+
+        for (int i = 0; i < _itens.Length; i++)
         {
-            Console.WriteLine("Aumentando a capacidade da lista!");
-            ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
-            for (int i = 0; i < _itens.Length; i++)
+            novoArray[i] = _itens[i];
+        }
+        _itens = novoArray;
+    }
+
+    public void Remover(ContaCorrente conta)
+    {
+        int indiceItem = -1;
+        for (int i = 0; i < _proximaPosicao; i++)
+        {
+            ContaCorrente contaAtual = _itens[i];
+            if (contaAtual == conta)
             {
-                novoArray[i] = _itens[1];
+                indiceItem = i;
+                break;
             }
-            _itens = novoArray;
+        }
+        // 0         1       2
+        //[conta1][conta2][conta4][conta5][null]
+        for (int i = indiceItem; i < _proximaPosicao - 1; i++)
+        {
+            _itens[i] = _itens[i + 1];
+        }
+        _proximaPosicao--;
+        _itens[_proximaPosicao] = null;
+    }
+
+    public void ExibirLista()
+    {
+        for (int i = 0; i < _itens.Length; i++)
+        {
+            if (_itens[i] != null)
+            {
+                var conta = _itens[i];
+                Console.WriteLine($" Indice[{i}] = " +
+                    $"Conta:{conta.Conta} - " +
+                    $"N° da Agência: {conta.Numero_agencia}");
+            }
         }
     }
+
+    public ContaCorrente RecuperarContaNoIndice(int indice)
+    {
+        if (indice < 0 || indice >= _proximaPosicao)
+        {
+            throw new ArgumentOutOfRangeException(nameof(indice));
+        }
+
+        return _itens[indice];
+    }
+
+    public int Tamanho
+    {
+        get
+        {
+            return _proximaPosicao;
+        }
+    }
+
+    public ContaCorrente this[int indice]
+    {
+        get
+        {
+            return RecuperarContaNoIndice(indice);
+        }
+    }
+
+
+
 }
